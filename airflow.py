f185557dcc0405d7a40e5b0aa943b80098d06f8b
San2809 <53544186+San2809@users.noreply.github.com>
2019-10-04 23:03:47 -0400

    Update airflow.py

--- airflow.py
+++ airflow.py
@@ -5,3 +5,21 @@ from airflow.operators.bash_operator import BashOperator
 
 from datetime import datetime, timedelta
 import iso8601
+
+PROJECT_HOME = os.environ["PROJECT_HOME"]
+EMAIL = os.environ["MY_EMAIL"]
+default_args = {
+  'owner': 'airflow',
+  'depends_on_past': False,
+  'start_date': iso8601.parse_date("2017-08-28"),
+  'email': [EMAIL],
+  'email_on_failure': True,
+  'email_on_retry': True,
+  'retries': 3,
+  'retry_delay': timedelta(7),
+}
+
+training_dag = DAG(
+  'weekly_model_training',
+  default_args=default_args
+)
